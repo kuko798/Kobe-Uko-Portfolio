3291fbd18515e3bce01f089160f400f69e7f4ca6
unknown <kobeuko@gmail.com>
2025-07-25 18:44:51 -0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
-base: '/Kobe-Uko-Portfolio/',
+base: '/',
 build: {
 outDir: 'docs'
 }
